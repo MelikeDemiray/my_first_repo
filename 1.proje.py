a33de1b4b3812c04a49cec25d1d49e0f7d0edfad
Melike Demiray <melikedemirayy23@gmail.com>
2025-01-15 14:32:40 +0100

    Create 1.proje.py

---
+++ 1.proje.py
@@ -0,0 +1,27 @@
+#Question1: Create a Python class called "Rectangle" that represents a rectangle. 
+#The Rectangle class must have the following properties and methods:
+
+#Features:
+#width (an integer)
+#height (an integer)
+#Methods:
+#area(self): A method that calculates and returns the area of ​​the rectangle.
+#perimeter(self): A method that calculates and returns the perimeter of the rectangle.
+#Create an instance of Rectangle class, set its width to 5 and height to 7, then print its area and perimeter.
+
+class Rectangle():
+
+    def __init__(self, weight, height):
+        self.weight=weight
+        self.height=height  
+
+    def area(self):
+        return self.weight * self.height
+    
+    def perimeter(self):
+        return (self.weight*2) + (self.height*2)
+    
+
+x=Rectangle(3,2)
+
+print(f"Rectangle area:{Rectangle.area(x)}\nRectangle perimeter:{Rectangle.perimeter(x)}")
